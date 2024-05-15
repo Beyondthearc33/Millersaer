92535454ce2bac883ccc9683952b464586bf1d52
NateElison18 <122648132+NateElison18@users.noreply.github.com>
2024-05-14 23:24:39 -0600

    Format gallery, started lightbox

---
+++ scripts/lightboxScript.js
@@ -0,0 +1,10 @@
+const lightbox = document.querySelector('#lightbox');
+const images = document.querySelectorAll('.gallery-image');
+let imageIndex = 0;
+ function openLightbox(event) {
+
+ };
+
+ function closeLightbox() {
+
+ }
