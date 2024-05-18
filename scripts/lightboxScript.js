7a5862e71bfed8b39c4be1a153748d0b83f6bd21
NateElison18 <122648132+NateElison18@users.noreply.github.com>
2024-05-18 17:28:38 -0600

    Gallery Scripts finished: next, prev, close btns

--- scripts/lightboxScript.js
+++ scripts/lightboxScript.js
@@ -1,10 +1,70 @@
 const lightbox = document.querySelector('#lightbox');
+const lightboxImage = document.querySelector('#lightboxImage');
 const images = document.querySelectorAll('.gallery-image');
-let imageIndex = 0;
- function openLightbox(event) {
 
+let imageIndex = 0;
+
+console.log(images[0].getAttribute('src'));
+setListeners();
+
+function setListeners() {
+    const nextImageBtn = document.querySelector('#rightBtn');
+    const previousImageBtn = document.querySelector('#leftBtn');
+    const closeBtn = document.querySelector('#closeBtn');
+
+    nextImageBtn.addEventListener('click', fetchNextImage);
+    previousImageBtn.addEventListener('click', fetchPreviousImage);
+    closeBtn.addEventListener('click', closeLightbox);
+
+    let indexToSet = 0; 
+
+    images.forEach(image => {
+        image.setAttribute('index', indexToSet);
+        image.addEventListener('click', () => {
+            openLightbox(image);
+        });
+        indexToSet++
+    });
+
+}
+
+function fetchNextImage() {
+    if(imageIndex == images.length - 1){
+        imageIndex = 0;
+    } else {
+        imageIndex++;
+    }
+    setLightboxImageByIndex();
+    console.log('Getting next image. . . ');
+}
+
+function fetchPreviousImage() {
+    if(imageIndex == 0) 
+        imageIndex = 11;
+    else
+        imageIndex--;
+
+    setLightboxImageByIndex();
+    console.log('Getting previous image. . . ');
+
+
+}
+
+function openLightbox(image) {
+    imageIndex = image.getAttribute('index');
+    console.log("trying to open lightbox at index " + imageIndex);
+    lightbox.setAttribute("style", "  visibility: visible;");
+    lightbox.setAttribute('index', imageIndex);
+    setLightboxImageByIndex();
  };
 
  function closeLightbox() {
-
+    lightbox.setAttribute("style", "visibility: hidden;");
+    imageIndex = 0;
  }
+
+ function setLightboxImageByIndex() {
+    let src = images[imageIndex].getAttribute('src');
+    lightboxImage.setAttribute('src', src);
+ }
+
